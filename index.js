d59d06a6e8faddd2438a682388e54247dd342fe7
AshakaE <ashakaegerega@gmail.com>
2021-08-03 14:33:30 +0100

    Separate codebase into module

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 const fs = require('fs');
 const http = require('http');
-const url = require('url');
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-}
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const overview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
 const card = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
